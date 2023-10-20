7d958e16dc627c15a7691838720a669f8f932eb3
gururajkulkarn <107684801+gururajkulkarn@users.noreply.github.com>
2023-10-20 17:54:41 +0530

    api link and key updated

--- src/CreateUser.jsx
+++ src/CreateUser.jsx
@@ -1,47 +1,79 @@
-import React, { useState } from 'react'
-import axios from 'axios'
-import {useNavigate} from 'react-router-dom'
-import { ToastContainer,toast} from 'react-toastify';
-import "react-toastify/dist/ReactToastify.css";
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const CreateUser = () => {
+  const [text, setText] = useState({
+    name: '',
+    email: '',
+    age: '',
+  });
 
-const [text,setText] = useState({
-  name:'',
-  email:'',
-  age:''
-})
+  const navigate = useNavigate();
 
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setText({ ...text, [name]: value });
+  };
 
-const navigate = useNavigate()
-
-const handlesubmit = (e) =>{
-e.preventDefault()
-axios.post('http://localhost:3001/createUser',text)
-.then(response => {
-  navigate('/home')
-}).catch(error => console.log(error))
-
-}
-
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post('https://ap-south-1.aws.data.mongodb-api.com/app/data-qkomn/endpoint/data/v1/createUser', text, {
+        headers: {
+          'Authorization': 'Bearer nUfQ0zaDktNoLKDy8uZZs3D9Csma0vVZfILfcVTLMi8XqGzoon0rWBHjRYbvZCn2',
+          'Content-Type': 'application/json',
+        },
+      })
+      .then((response) => {
+        navigate('/home');
+        toast.success('User created successfully');
+      })
+      .catch((error) => {
+        console.error(error);
+        toast.error('Error creating user');
+      });
+  };
 
   return (
-    <> 
-    <ToastContainer />
-    <div className='container'>
-      <h1>Add User  </h1>  
-<form onSubmit={handlesubmit}>
-    <input type="text" className='form-control  w-25 m-3'  name="name"  placeholder='Name'  onChange={e =>setText({...text,name: e.target.value})}/>
-    <input type="text" className='form-control  w-25 m-3'  name="email" placeholder='Email' onChange={e =>setText({...text,email: e.target.value})}/>
-    <input type="text" className='form-control  w-25 m-3'  name="age"   placeholder='Age'   onChange={e =>setText({...text,age: e.target.value})}/>
-    <button className='btn btn-success m-3'>Submit</button>
-</form>
-
-    </div>
+    <>
+      <ToastContainer />
+      <div className='container'>
+        <h1>Add User</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type='text'
+            className='form-control w-25 m-3'
+            name='name'
+            placeholder='Name'
+            value={text.name}
+            onChange={handleInputChange}
+          />
+          <input
+            type='text'
+            className='form-control w-25 m-3'
+            name='email'
+            placeholder='Email'
+            value={text.email}
+            onChange={handleInputChange}
+          />
+          <input
+            type='text'
+            className='form-control w-25 m-3'
+            name='age'
+            placeholder='Age'
+            value={text.age}
+            onChange={handleInputChange}
+          />
+          <button className='btn btn-success m-3' type='submit'>
+            Submit
+          </button>
+        </form>
+      </div>
     </>
-  )
+  );
+};
 
-
-}
-
-export default CreateUser
+export default CreateUser;
